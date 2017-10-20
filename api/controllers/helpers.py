50b807d877213a5d4d17ccfe8626f2c92ddcab97
Wahyu Prihantoro <wahyuprihantoro21@gmail.com>
2017-10-20 10:10:27 +0700

    login with jwt

--- api/controllers/helpers.py
+++ api/controllers/helpers.py
@@ -1,3 +1,8 @@
+from rest_framework_jwt.settings import api_settings
+
+from api.serializers import UserSerializer
+
+
 def success_context(**kwargs):
     context = {
         'status': True,
@@ -22,3 +27,21 @@ def fatal_context():
         'message': 'terjadi kesalahan pada server'
     }
     return context
+
+
+def generate_token(user):
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+    payload = jwt_payload_handler(user)
+    return jwt_encode_handler(payload)
+
+
+def get_user_data(header):
+    jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
+    return jwt_decode_handler(header.get('HTTP_AUTHORIZATION').split(' ')[1])
+
+
+def construct_login_return_content(user):
+    token = generate_token(user)
+    user_data = UserSerializer(user).data
+    return success_context(user=user_data, token=token)
