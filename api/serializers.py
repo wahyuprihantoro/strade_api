e189ccb8e1d7757522838060fa8e9bb92fe056f6
Wahyu Prihantoro <wahyuprihantoro21@gmail.com>
2017-10-20 10:42:08 +0700

    change model

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,4 @@ from api.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role', 'phone_number']
+        fields = ['id', 'username', 'full_name', 'email', 'role', 'phone_number', 'store_name', 'store_category']
